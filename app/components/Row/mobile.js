792baabaac74ad6d4586665c9f1882dfc6774aaf
co2-git <francoisrvespa@gmail.com>
2016-11-13 21:23:08 -0500

    Fix typo in mobile

--- app/components/Row/mobile.js
+++ app/components/Row/mobile.js
@@ -4,7 +4,7 @@ import {View} from 'react-native';
 export default function RowMobile(props) {
   const style = [
     {
-      flexDisplay: 'row'
+      flexDirection: 'row',
     },
     props.style,
   ];
