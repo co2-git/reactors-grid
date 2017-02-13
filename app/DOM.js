c6ee475d527852df4c3e3f94b91ceea21066cb66
co2-git <francoisrvespa@gmail.com>
2017-02-12 22:59:56 -0500

    Add display flex for DOM

--- app/DOM.js
+++ app/DOM.js
@@ -5,6 +5,7 @@ export default class ReactorsGridDOM extends Component {
   render() {
     const props = omit(this.props, ['__ReactorsGridStyle']);
     const style = {
+      display: 'flex',
       ...omit(this.props.__ReactorsGridStyle, ['flex']),
       ...this.props.style,
     };
